e9627853d8a174970be0c41f2725f2913d59f590
Oscar Nilsson <oscar@oncolour.se>
2018-03-02 16:22:18 +0100

    adding blog wp base

---
+++ template-parts/header/header-image.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Displays header media
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+    <?php the_custom_header_markup(); ?>
